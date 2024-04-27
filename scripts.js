2bed40b8cde74d0ad7501fdfac0199bd480a5063
Fabien Brocklesby <fabienbrocklesby@icloud.com>
2024-04-27 21:17:17 +1200

    Contact page, and footer

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
-function setActiveClass() {
-	let navLinks = document.querySelectorAll("nav ul li");
-	let sections = document.querySelectorAll("header, section");
+document.addEventListener("DOMContentLoaded", function () {
+	function setActiveClass() {
+		let navLinks = document.querySelectorAll("nav ul li");
+		let sections = document.querySelectorAll("header, section");
 
-	sections.forEach((section, index) => {
-		let rect = section.getBoundingClientRect();
-		if (rect.top < window.innerHeight * 0.7 && rect.bottom > 0) {
-			navLinks.forEach((nav) => nav.classList.remove("active"));
-			navLinks[index].classList.add("active");
-		}
+		sections.forEach((section, index) => {
+			let rect = section.getBoundingClientRect();
+			if (rect.top < window.innerHeight * 0.7 && rect.bottom > 0) {
+				navLinks.forEach((nav) => nav.classList.remove("active"));
+				navLinks[index].classList.add("active");
+			}
+		});
+	}
+
+	setActiveClass();
+
+	window.addEventListener("scroll", setActiveClass);
+
+	document.getElementById("map").addEventListener("load", function () {
+		document.getElementById("gradient-border").style.display = "block";
 	});
-}
-
-setActiveClass();
-
-window.addEventListener("scroll", setActiveClass);
+});
