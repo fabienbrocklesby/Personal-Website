e99b139070b2cc128b98cca89a104e099f7a07fd
Fabien Brocklesby <fabienbrocklesby@icloud.com>
2024-04-27 21:47:49 +1200

    Finishing touches (more to come)

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+	AOS.init({
+		startEvent: "DOMContentLoaded",
+		duration: 1500,
+	});
+
 	function setActiveClass() {
 		let navLinks = document.querySelectorAll("nav ul li");
 		let sections = document.querySelectorAll("header, section");
 
 		sections.forEach((section, index) => {
 			let rect = section.getBoundingClientRect();
-			if (rect.top < window.innerHeight * 0.7 && rect.bottom > 0) {
+			if (rect.top >= 0 && rect.top < window.innerHeight / 2) {
 				navLinks.forEach((nav) => nav.classList.remove("active"));
 				navLinks[index].classList.add("active");
+
+				// Update the URL
+				let id = section.getAttribute("id");
+				history.pushState(null, null, "#" + id);
 			}
 		});
 	}
@@ -18,5 +27,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	document.getElementById("map").addEventListener("load", function () {
 		document.getElementById("gradient-border").style.display = "block";
+	});
+
+	document.querySelectorAll("a").forEach((a) => {
+		a.addEventListener("click", function (e) {
+			e.preventDefault();
+			let target = document.querySelector(this.getAttribute("href"));
+			window.scrollTo({
+				top:
+					target.offsetTop - window.innerHeight / 2 + target.offsetHeight / 2,
+				behavior: "smooth",
+			});
+		});
 	});
 });
